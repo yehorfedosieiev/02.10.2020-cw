9f806cbd246b7c28eff1bf55b9258d5fadfebadf
Yehor Fedosieiev <pifagorka@gmail.com>
2020-10-04 12:11:42 +0300

    small changes (adding comments)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
+
 $("body").on("click", "[data-editable]", function(event){
 	event.preventDefault(); //
 	console.log(this);
 	const type = $(this).attr("data-editable");
-	console.log(type);
-
+	console.log("Значение data-editable", type);
+	
 	const $element = $(this);
-
+	console.log("текст в теге", $element.text());
 	$("<input>")
 		.attr("type", type)
 		.insertAfter($element)
@@ -16,17 +17,19 @@ $("body").on("click", "[data-editable]", function(event){
 			console.log("event", event);
 			switch (event.which){
 				case 13:
+					console.log("новый this ", this)
 					$element
 						.insertAfter(this)
 						.text($(this).val())
-					
+					console.log("значение this ", $(this).val())
 					$(this).remove();
 					break;
-				case 27: break;
+				case 27:
+					$element.insertAfter(this);
+					$(this).remove();
+					break;
 			}
 		})
-
-
 
 	$element.remove();
 });
